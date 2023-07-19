03b251a2d7b4c7ed1fe233f2c8d86eea6510cbb1
Gerardo Rodríguez <acidbjazz@gmail.com>
2023-07-19 13:02:32 -0500

    feat: logo + issue

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,15 @@
 import styles from "@/styles/header.module.scss";
 
 import Link from "next/link";
-import SVG from "@/components/svg";
+import Logo from "@/components/logo";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link href="/">
-        <SVG name="logoQ" width={34} />
-      </Link>
+      {/* <Link href="/"> */}
+      {/* <SVG name="logoQ" width={34} /> */}
+      <Logo />
+      {/* </Link> */}
     </header>
   );
 }
---
+++ components/logo.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import styles from "@/styles/logo.module.scss";
+
+import { useParams } from "next/navigation";
+import Link from "next/link";
+import SVG from "@/components/svg";
+
+export default function Header() {
+  const params = useParams();
+  console.log(params);
+  const issue = params.slug ? <div className={styles.issue}>{params.numero}</div> : null;
+  return (
+    <Link href={`/${params.slug ? params.numero : ""}`} className={styles.logo}>
+      <SVG name="logoQ" width={34} />
+      {issue}
+    </Link>
+  );
+}
